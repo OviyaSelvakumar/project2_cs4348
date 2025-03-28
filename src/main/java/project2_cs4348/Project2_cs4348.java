bf194a726cdf68599e0feb0b2cfbe441f0b1dcbf
oviya selvakumar <oviyaselvakumar13@gmail.com>
2025-03-27 22:11:41 -0500

    Initial Commit

---
+++ src/main/java/project2_cs4348/Project2_cs4348.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project2_cs4348;
+
+/**
+ *
+ * @author oviya
+ */
+public class Project2_cs4348 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
